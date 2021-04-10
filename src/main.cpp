3dc72266a7a8e6f15d6f755d9f4162d318bb7061
Roman <rsheyko1997@gmail.com>
2021-04-11 02:20:37 +0300

    deleting includes

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vulkan/vulkan.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_vulkan.h>
+#include <vector>
+#include "VulkanRenderer.h"
+#include "Allocator.h"
+#include "SDLSubsystem.h"
+#define RENDERER VulkanRenderer
+
+int main(int argc, char* argv[]) {
+	try {
+		SDLSubsystem windowSubsystem("Test", 640, 480);
+		windowSubsystem.mainLoop();
+	}
+	catch (RendererException & e) {
+		std::cout << e.what() << std::endl;
+	}
+    catch (WindowSubsystemException& e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+
+	return EXIT_SUCCESS;
+}
